95514e39b033d18b2816baa42b695b4b7adec5ed
enriquegt02 <72170859+enriquegt02@users.noreply.github.com>
2020-12-15 08:37:43 -0600

    Agenda
    Proyecto

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<?php require_once "scripts.php";  ?>
+</head>
+<body>
+	<?php require_once 'menu.php'; ?>
+	<div class="container-fluid">
+		<div class="row">
+		<div class="col-md-5">
+		<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="tinoco.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="itzel.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="sandra.jpg" alt="Third slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="nini.jpg" alt="Third slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" height="550" src="carlos.jpg" alt="Third slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" height="550" src="luis.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+       </div>
+			
+			<div class="col-md-6">
+			<ul class="list-group">
+           <li class="list-group-item d-flex justify-content-between align-items-center">
+             Fecha de entrega
+              <span class="badge badge-primary badge-pill">15-12-2020</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+			 Integrantes
+			 <ul class="list-group">
+
+               <li class="list-group-item"><span class="badge badge-primary badge-pill">Blanco Solano Sandra Melina</span></li>
+                <li class="list-group-item"><span class="badge badge-primary badge-pill">Bustamante Gandi Ninive </span></li>
+               <li class="list-group-item"><span class="badge badge-primary badge-pill">Gutierrez Tinoco Enrique</span></li>
+			  <li class="list-group-item"><span class="badge badge-primary badge-pill">Ramirez Galicia Ttzel</span></li>
+			  <li class="list-group-item"><span class="badge badge-primary badge-pill">Rodriguez Juarez Luis Enrique</span></li>
+                <li class="list-group-item"><span class="badge badge-primary badge-pill">Sanchez Vazquez Carlos Andres</span> </li>
+                </ul>
+	       
+           </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+             Equipo
+            <span class="badge badge-primary badge-pill">Con todo menos con miedo</span>
+             </li>
+             <li class="list-group-item d-flex justify-content-between align-items-center">
+             Materia
+             <span class="badge badge-primary badge-pill">Programacion MVC</span>
+            </li>
+           </ul>
+			</div>
+		
+
+		</div>
+
+
+
+
+    </div>
+</body>
+</html>
+
+
